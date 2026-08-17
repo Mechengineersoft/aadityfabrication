1f9dd8783c68e510d768fbcb62610af876757706
Mechengineersoft <user@example.com>
2026-08-17 19:51:35 +0530

    owner email changed

--- artifacts/api-server/src/lib/mailer.ts
+++ artifacts/api-server/src/lib/mailer.ts
@@ -1,6 +1,6 @@
 import { logger } from "./logger";
 
-const OWNER_EMAIL = "mechengineersoft@gmail.com";
+const OWNER_EMAIL = "zeeshan49ahmad@gmail.com";
 
 interface InquiryDetails {
   name: string;

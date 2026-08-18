107cd556f00170d940ab18cb85de6e72d043f4cf
Mechengineersoft <user@example.com>
2026-08-18 20:14:53 +0530

    in admin phone number changed and email for both quotation and admin password changed

--- artifacts/api-server/src/lib/mailer.ts
+++ artifacts/api-server/src/lib/mailer.ts
@@ -1,6 +1,6 @@
 import { logger } from "./logger";
 
-const OWNER_EMAIL = "zeeshan49ahmad@gmail.com";
+const OWNER_EMAIL = "aadityfabricationworks@gmail.com";
 
 interface InquiryDetails {
   name: string;
